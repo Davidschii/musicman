45ca23765b24612f7e02c60347f3f1831c246035
Davidschi <davidschi420@gmail.com>
2023-02-14 22:56:21 +0100

    updated main.py, added .gitignore and music folder

--- main.py
+++ main.py
@@ -1,18 +1,42 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
-from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
 
 
-class Window(QWidget):
+
+class Player(QWidget):
     def __init__(self):
         QWidget.__init__(self)
         self.resize(1280,720)
         self.setWindowTitle("Musicman")
+
+        # Widgets
         self.setWindowIcon(QIcon("assets/icon.png"))
+        self.button_play = QPushButton("Play", self)
+        self.songdir = QLineEdit(self)
+
+        # Layout
+        hbl = QHBoxLayout()
+        hbl.addWidget(self.button_play)
+
+        vbl = QVBoxLayout()
+        vbl.addWidget(self.songdir)
+        vbl.addLayout(hbl)
+
+        self.setLayout(vbl)
+
+        # Connections
+        self.songdir.returnPressed.connect(self.songplay)
+        self.button_play.clicked.connect(self.songplay)
+
         self.show()
+
+    def songplay(self):
+        sys.exit()
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    w = Window()
+    w = Player()
     sys.exit(app.exec_())
