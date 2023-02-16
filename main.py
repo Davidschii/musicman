b8c3c566e8af21dd355ffd86864baf5ba25f2e17
Davidschi <davidschi420@gmail.com>
2023-02-16 16:17:37 +0100

    added tray functionality

--- main.py
+++ main.py
@@ -7,16 +7,25 @@ from PyQt5.QtGui import *
 class Player(QWidget):
     def __init__(self):
         super().__init__()
-#        self.resize(1280,720)
         self.setWindowTitle("Musicman")
         self.song_directory = "music/"
+        self.icon = "assets/icon.png"
         pygame.mixer.init()
 
+        # Tray Icon
+        self.trayicon = QSystemTrayIcon(QIcon(self.icon))
+        self.trayicon.setToolTip("Musicman")
+        traymenu = QMenu()
+        trayopen = traymenu.addAction("Open")
+        trayexit = traymenu.addAction("Exit")
+        self.trayicon.setContextMenu(traymenu)
+
         # Widgets
-        self.setWindowIcon(QIcon("assets/icon.png"))
+        self.setWindowIcon(QIcon(self.icon))
         self.label = QLabel("Song selection:", self)
         self.button_play = QPushButton("Play", self)
         self.button_pause = QPushButton("Pause / Resume", self)
+        self.button_min = QPushButton("Minimize to tray", self)
         self.songlistbox = QComboBox(self)
 
         # Layout
@@ -24,10 +33,14 @@ class Player(QWidget):
         hbl.addWidget(self.label)
         hbl.addWidget(self.songlistbox)
 
+        hbl2 = QHBoxLayout()
+        hbl2.addWidget(self.button_play)
+        hbl2.addWidget(self.button_pause)
+
         vbl = QVBoxLayout()
         vbl.addLayout(hbl)
-        vbl.addWidget(self.button_play)
-        vbl.addWidget(self.button_pause)
+        vbl.addLayout(hbl2)
+        vbl.addWidget(self.button_min)
 
         self.setLayout(vbl)
         self.songlist_init()
@@ -37,7 +50,9 @@ class Player(QWidget):
         self.songlistbox.activated[str].connect(self.song_change)
         self.button_play.clicked.connect(self.song_play)
         self.button_pause.clicked.connect(self.song_pause)
-
+        self.button_min.clicked.connect(self.minimize)
+        trayopen.triggered.connect(self.minimize)
+        trayexit.triggered.connect(self.stop)
         self.show()
 
     def songlist_init(self):
@@ -65,6 +80,18 @@ class Player(QWidget):
             pygame.mixer.music.pause()
         else:
             pygame.mixer.music.unpause()
+
+    def minimize(self):
+        if self.isVisible():
+            self.trayicon.show()
+            self.hide()
+        else:
+            self.trayicon.hide()
+            self.show()
+
+    def stop(self):
+        pygame.mixer.quit()
+        sys.exit()
 
 
 if __name__ == "__main__":
