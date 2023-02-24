f110795ee78751db1f4b08faeccd5b8529f2c882
Davidschi <davidschi420@gmail.com>
2023-02-24 22:00:17 +0100

    added directory support, added stylesheet.qss, improved code

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
-# musicman - WIP
+# musicman
 A simple music player written in Python using PyQt5
 
 Supports mp3, wav and flac files on Windows
 
-Python project for a school research paper
+Reads the audio files in the "C:\Users\<Username>\Music" folder by default
 
-Please put the songs in the player's music folder
+Python project for a school research paper by David Dubinin
 
-![ ](https://github.com/Davidschii/musicman/blob/master/assets/icon.png?raw=true)
+
+![ ](https://github.com/Davidschii/musicman/blob/master/assets/icon-512.png?raw=true)
--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from mutagen.flac import FLAC
 from PyQt5.QtCore import Qt, QUrl
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
-from PyQt5.QtMultimedia import QMediaPlayer, QMediaContent, QMediaPlaylist
+from PyQt5.QtMultimedia import QMediaPlayer, QMediaContent
 
 # changes QMediaPlayer backend
 os.environ['QT_MULTIMEDIA_PREFERRED_PLUGINS'] = 'windowsmediafoundation'
@@ -18,56 +18,77 @@ class Player(QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Musicman")
-        self.song_directory = "music/"
         self.icon = "assets/icon.png"
-        self.songliststart = "Please choose a song."
+        self.setWindowIcon(QIcon(self.icon))
+        self.songliststart = "Please choose an audio file."
         self.songname = self.songliststart
+        self.resize(500,150)
         self.musicplayer = QMediaPlayer(self)
 
-        # Widgets
-        self.setWindowIcon(QIcon(self.icon))
-        self.progressSlider = QSlider(Qt.Horizontal)
+        # set stylesheet
+        with open("stylesheet.qss", "r") as stylesheet:
+            self.setStyleSheet(stylesheet.read())
+
+        # set file directory
+        with open("directory", "r") as dirfile:
+            self.song_directory = dirfile.readline()
+            if os.path.getsize("directory") == 0:
+                self.song_directory = f"C:\\Users\{os.getlogin()}\\Music\\"
+
+        self.createWidgets()
+        self.createTray()
+        self.createLayout()
+        self.dirUpdate()
+
+        # Connections
+        self.customdirectory.returnPressed.connect(self.dirUpdate)
+        self.customdirectoryupdate.clicked.connect(self.dirUpdate)
+        self.songlistbox.activated[str].connect(self.songChange)
+        self.button_play.clicked.connect(self.songPlay)
+        self.button_pause.clicked.connect(self.songPause)
+        self.button_min.clicked.connect(self.minimize)
+        self.musicplayer.positionChanged.connect(self.positionUpdate)
+        self.progressbar.sliderMoved.connect(self.progressBar)
+
+        self.trayopen.triggered.connect(self.minimize)
+        self.trayexit.triggered.connect(sys.exit)
+
+        self.show()
+
+    def createWidgets(self):
+        self.customdirectory = QLineEdit(self.song_directory, self)
+        self.customdirectoryupdate = QPushButton("Update directory", self)
+        self.progressbar = QSlider(Qt.Horizontal)
         self.label = QLabel("Song selection:", self)
         self.button_play = QPushButton("Play", self)
-        self.button_pause = QPushButton("Pause / Resume", self)
+        self.button_pause = QPushButton("", self)
+        self.button_pause.setIcon(QIcon("assets/pause.png"))
         self.button_min = QPushButton("Minimize to tray", self)
         self.songlistbox = QComboBox(self)
         self.timelabel1 = QLabel("00:00", self)
         self.timelabel2 = QLabel("00:00", self)
 
-        # Tray
-        self.trayicon = QSystemTrayIcon(QIcon(self.icon))
-        self.trayicon.setToolTip("Musicman")
-        traymenu = QMenu()
-        trayopen = traymenu.addAction("Open")
-        trayexit = traymenu.addAction("Exit")
-        self.trayicon.setContextMenu(traymenu)
+    def createLayout(self):
 
-        # Songlist
-        start = len(self.song_directory)
-        songlist = [self.song_directory + self.songliststart]
-        songlist = songlist + glob(self.song_directory + "*.mp3") + glob(self.song_directory + "*.wav") \
-                            + glob(self.song_directory + "*.flac")
-        for song in songlist:
-            self.songlistbox.addItem(song[start:])
-
-
-        # Layout
         hbl = QHBoxLayout()
         hbl.addWidget(self.label)
         hbl.addWidget(self.songlistbox)
 
         hbl2 = QHBoxLayout()
         hbl2.addWidget(self.timelabel1)
-        hbl2.addWidget(self.progressSlider)
+        hbl2.addWidget(self.progressbar)
         hbl2.addWidget(self.timelabel2)
 
         hbl3 = QHBoxLayout()
         hbl3.addWidget(self.button_play)
         hbl3.addWidget(self.button_pause)
 
+        hbl4 = QHBoxLayout()
+        hbl4.addWidget(self.customdirectory)
+        hbl4.addWidget(self.customdirectoryupdate)
 
         vbl = QVBoxLayout()
+        vbl.addLayout(hbl4)
         vbl.addLayout(hbl)
         vbl.addLayout(hbl2)
         vbl.addLayout(hbl3)
@@ -75,21 +96,31 @@ class Player(QWidget):
 
         self.setLayout(vbl)
 
-        # Connections
-        self.songlistbox.activated[str].connect(self.song_change)
-        self.button_play.clicked.connect(self.song_play)
-        self.button_pause.clicked.connect(self.song_pause)
-        self.button_min.clicked.connect(self.minimize)
-        self.musicplayer.positionChanged.connect(self.positionupdate)
-        self.progressSlider.sliderMoved.connect(self.progress_Slider)
+    def createTray(self):
+        self.trayicon = QSystemTrayIcon(QIcon(self.icon))
+        self.trayicon.setToolTip("Musicman")
+        traymenu = QMenu()
+        self.trayopen = traymenu.addAction("Open")
+        self.trayexit = traymenu.addAction("Exit")
+        self.trayicon.setContextMenu(traymenu)
 
-        trayopen.triggered.connect(self.minimize)
-        trayexit.triggered.connect(sys.exit)
+    def dirUpdate(self):
+        self.songlistbox.clear()
+        self.song_directory = str(self.customdirectory.text())
+        if self.song_directory[-1:] != "\\":
+            self.song_directory = self.song_directory + "\\"
+        start = len(self.song_directory)
+        songlist = [self.song_directory + self.songliststart]
+        songlist = songlist + glob(self.song_directory + "*.mp3") + glob(self.song_directory + "*.wav") \
+                   + glob(self.song_directory + "*.flac")
+        for song in songlist:
+            self.songlistbox.addItem(song[start:])
+        with open("directory", "w") as dirfile:
+            dirfile.write(self.song_directory)
 
-        self.show()
 
-    def song_change(self):
-        self.songname = str(self.songlistbox.currentText())
+    def songChange(self):
+        self.songname = self.songlistbox.currentText()
         if self.songname != self.songliststart:
             self.song = self.song_directory + self.songname
         try:
@@ -103,7 +134,7 @@ class Player(QWidget):
             self.length = int(audio_info.length)
         except mutagen.MutagenError:
             self.length = 0
-        hours, mins, secs = (self.timeconv(self.length))
+        hours, mins, secs = (self.timeConv(self.length))
         hours = str(hours)
         mins = str(mins)
         secs = str(secs)
@@ -117,19 +148,25 @@ class Player(QWidget):
             self.timelabel2.setText(f"{mins}:{secs}")
         else:
             self.timelabel2.setText(f"{hours}:{mins}:{secs}")
-        self.progressSlider.setRange(0, self.length)
-        self.progressSlider.setValue(0)
+        self.progressbar.setRange(0, self.length)
+        self.progressbar.setValue(0)
 
-    def song_play(self):
+    def songPlay(self):
         try:
             self.musicplayer.setMedia(QMediaContent(QUrl.fromLocalFile(self.song)))
             self.musicplayer.play()
         except AttributeError:
             pass
 
-    def positionupdate(self, position):
+    def songPause(self):
+        if self.musicplayer.state() == QMediaPlayer.PlayingState:
+            self.musicplayer.pause()
+        else:
+            self.musicplayer.play()
+
+    def positionUpdate(self, position):
         self.position = round(position / 1000)
-        hours, mins, secs = self.timeconv(self.position)
+        hours, mins, secs = self.timeConv(self.position)
         hours = str(hours)
         mins = str(mins)
         secs = str(secs)
@@ -143,16 +180,10 @@ class Player(QWidget):
             self.timelabel1.setText(f"{mins}:{secs}")
         else:
             self.timelabel1.setText(f"{hours}:{mins}:{secs}")
-        self.progressSlider.setValue(self.position)
+        self.progressbar.setValue(self.position)
 
-    def progress_Slider(self, position):
+    def progressBar(self, position):
         self.musicplayer.setPosition(position * 1000)
-
-    def song_pause(self):
-        if self.musicplayer.state() == QMediaPlayer.PlayingState:
-            self.musicplayer.pause()
-        else:
-            self.musicplayer.play()
 
     def minimize(self):
         if self.isVisible():
@@ -162,7 +193,7 @@ class Player(QWidget):
             self.trayicon.hide()
             self.show()
 
-    def timeconv(self, length):
+    def timeConv(self, length):
         hours = int(length / 3600)
         minutes = int(length / 60 - hours * 60)
         seconds = round(length - hours * 3600 - minutes * 60)
